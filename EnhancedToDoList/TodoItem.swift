d383be47861b1396398ce4e181934a1b4a389a0e
Justin Wei <jwei@lcs.on.ca>
2024-01-19 14:48:56 -0500

    Can now delete items from the list

--- EnhancedToDoList/TodoItem.swift
+++ EnhancedToDoList/TodoItem.swift
@@ -4,5 +4,36 @@
 //
 //  Created by Justin Zack Wei on 2024-01-19.
 //
-
+import Observation
 import Foundation
+
+@Observable
+class TodoItem: Identifiable {
+    
+    let id = UUID()
+    var details: String
+    let createdOn: Date
+    var isCompleted: Bool
+    var completedOn: Date?
+    
+    init(
+        details: String,
+        createdOn: Date = Date(),
+        isCompleted: Bool = false,
+        completedOn: Date? = nil
+    ) {
+        self.details = details
+        self.createdOn = createdOn
+        self.isCompleted = isCompleted
+        self.completedOn = completedOn
+    }
+
+}
+
+let exampleData = [
+    
+    TodoItem(details: "Go for a walk"),
+    TodoItem(details: "Study for Physics"),
+    TodoItem(details: "Call mom"),
+    
+]
--- EnhancedToDoList/TodoListView.swift
+++ EnhancedToDoList/TodoListView.swift
@@ -8,11 +8,84 @@
 import SwiftUI
 
 struct TodoListView: View {
+    
+    // MARK: Stored properties
+    @State private var newItemDetails = ""
+    // Our list of items to complete
+    @State private var items: [TodoItem] = []
+    
+    // MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                HStack {
+                    
+                    TextField("Enter a to-do item", text: $newItemDetails)
+                    
+                    Button("Add") {
+                        addItem()
+                    }
+
+                }
+                .padding(20)
+                if items.isEmpty {
+                    
+                    ContentUnavailableView(label: {
+                        Label(
+                            "Nothing to do",
+                            systemImage: "powersleep"
+                        )
+                        .foregroundStyle(.green)
+                    }, description: {
+                        Text("To-do items will appear here once you add some.")
+                    })
+                    
+                } else {
+                    List{
+                        ForEach(items) { currentItem in
+                            Label {
+                                Text(currentItem.details)
+                            } icon: {
+                                Image(systemName: currentItem.isCompleted ? "checkmark.circle" : "circle")
+                                    .onTapGesture {
+                                        toggle(item: currentItem)
+                                    }
+                            }
+                        }
+                        .onDelete(perform: delete)
+                    }
+                }
+            }
+            .navigationTitle("Tasks")
+        }
+        .onAppear {
+            // Populate with example data
+            if items.isEmpty {
+                items.append(contentsOf: exampleData)
+            }
+        }
+    }
+    // MARK: Functions
+    func addItem() {
+        let newToDoItem = TodoItem(details: newItemDetails)
+        items.insert(newToDoItem, at: 0)
+        newItemDetails = ""
+    }
+    func toggle(item: TodoItem) {
+        if item.isCompleted {
+            item.completedOn = nil
+            item.isCompleted = false
+        } else {
+            item.completedOn = Date()
+            item.isCompleted = true
+        }
+        
+    }
+    func delete(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
     }
 }
 
 #Preview {
-    TodoListView()
+    LandingView()
 }
