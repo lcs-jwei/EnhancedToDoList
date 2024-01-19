62d8ab13bf260e996a652aa9a6c78090941e8a8f
Justin Wei <jwei@lcs.on.ca>
2024-01-19 14:30:28 -0500

    Initial Commit

---
+++ EnhancedToDoList/EnhancedToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedToDoListApp.swift
+//  EnhancedToDoList
+//
+//  Created by Justin Zack Wei on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
