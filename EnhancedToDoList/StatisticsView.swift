ab82c04950fff7d5c616f3ee5e40e367e207f0d1
Justin Wei <jwei@lcs.on.ca>
2024-01-19 14:35:59 -0500

    Added tab views

---
+++ EnhancedToDoList/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  EnhancedToDoList
+//
+//  Created by Justin Zack Wei on 2024-01-19.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
---
+++ EnhancedToDoList/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  EnhancedToDoList
+//
+//  Created by Justin Zack Wei on 2024-01-19.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoListView()
+}
