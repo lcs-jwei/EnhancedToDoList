d5d5214e93cb5062e947380cdef396d60359504b
Justin Wei <jwei@lcs.on.ca>
2024-01-19 14:32:49 -0500

    Initial project organization

---
+++ EnhancedToDoList/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedToDoList
+//
+//  Created by Justin Zack Wei on 2024-01-19.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
